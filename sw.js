3d0c9437a3f6ff844f46884221a07a21ab41d0b4
albertjafe <albertjafe@gmail.com>
2026-08-27 16:04:43 +0200

    Cache auto-growing iPad task drawer

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = 'estudio-v285';
+const CACHE = 'estudio-v286';
 const ASSETS = [
   './index.html',
   './styles.css?v=278',
   './piano-rooms.css?v=1',
   './crono-readiness-layout.css?v=1',
-  './crono-idle-hierarchy.css?v=2',
+  './crono-idle-hierarchy.css?v=3',
   './app.js?v=278',
   './piano-rooms.js?v=1',
   './piano-rooms-core.js?v=1',
