ca7100d8989b279468857197a832aa9a89db5392
albertjafe <albertjafe@gmail.com>
2026-09-03 12:12:59 +0200

    chore: refresh planning v4 asset

--- piano-rooms.js
+++ piano-rooms.js
@@ -60,7 +60,7 @@
       load('eventPlanningUiV2Script','./event-planning-ui-v2.js?v=1',function(){
         load('planningEnhancementsV3Script','./planning-enhancements-v3.js?v=1',function(){
           load('planningEnhancementsV3FixScript','./planning-enhancements-v3-fix.js?v=1',function(){
-            load('planningEnhancementsV4Script','./planning-enhancements-v4.js?v=1');
+            load('planningEnhancementsV4Script','./planning-enhancements-v4.js?v=2');
           });
         });
       });
