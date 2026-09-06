0666f939b1133a295ae0afeed4a8fe1fd4b08a39
albertjafe <albertjafe@gmail.com>
2026-09-06 10:30:59 +0200

    Load General passage tracker v358

--- crono-resume-layout.js
+++ crono-resume-layout.js
@@ -134,7 +134,7 @@ function loadPianoCompanion(id, src, cssId, cssHref) {
 (function loadPassageTracker() {
   'use strict';
   if (window.PassageTracker) return;
-  loadPianoCompanion('passageTrackerScript', './passage-tracker.js?v=355', 'passageTrackerStyles', './passage-tracker.css?v=355');
+  loadPianoCompanion('passageTrackerScript', './passage-tracker.js?v=358', 'passageTrackerStyles', './passage-tracker.css?v=355');
 }());
 
 /* Eventos: búsqueda de repertorio pensada para bibliotecas grandes. */
